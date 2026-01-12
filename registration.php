6eac00b7cbc0454dfafbe64c116af891f2d6702f
Maurits Meester <mmeester@users.noreply.github.com>
2026-01-12 13:38:16 +0100

    feat(mask): implement alpine mask plugin for hyva usage

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Implementation by Deadsimple ecommerce - https://github.com/Deadsimple-ecommerce
+ * Copyright © Deadsimple ecommerce. All rights reserved.
+ *
+ * See LICENSE for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Deadsimple_HyvaInputMask',
+    __DIR__
+);
